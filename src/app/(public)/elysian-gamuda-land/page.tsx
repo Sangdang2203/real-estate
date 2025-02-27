4fea0b61685f817d1b7007cb2bebb663d80ff234
Đặng Văn Sang <122693847+Sangdang2203@users.noreply.github.com>
2025-02-27 16:33:11 +0700

    Update page.tsx

--- src/app/(public)/elysian-gamuda-land/page.tsx
+++ src/app/(public)/elysian-gamuda-land/page.tsx
@@ -7,7 +7,8 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Elysian by Gamuda Land",
   keywords: ["Elysian", "Gamuda Land", "Elysian city", "can ho elysian"],
-  description: "",
+  description:
+    "Về với Elysian, chúng ta tạm rời xa cuộc sống hối hả và ồn ào của đô thị để hòa mình cùng thiên nhiên đậm chất cây rợp bóng, cùng thưởng ngoạn cảnh sắc thiên nhiên yên bình, nơi ngồi thư giãn bên hồ bơi, hàn huyên cùng gia đình và bạn bè và hưởng thụ những tiện ích nội khu đa dạng và phong phú như: hồ bơi đạt tiêu chuẩn Olympic, vườn giữa tán khổng lồ, khu tập golf mini, sân thể thao chức năng, khu sân chơi trẻ em, “nông trại tại gia”, tổ hợp Shophouse, v.v...",
 };
 
 export default function Page() {
